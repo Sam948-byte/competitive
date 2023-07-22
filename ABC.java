ab7619f8174490175b0929768fdbcb0295126936
--global <--global>
2023-07-22 16:29:42 -0500

    ABC and statues

---
+++ ABC.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class ABC {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int first = in.nextInt();
+        int second = in.nextInt();
+        int third = in.nextInt();
+
+        String order = in.next();
+
+        int[] nums = new int[3];
+
+        if(first < second && first < third) {
+            nums[0] = first;
+            if(second < third) {
+                nums[1] = second;
+                nums[2] = third;
+            }else{
+                nums[1] = third;
+                nums[2] = second;
+            }
+    }else if(second < first && second < third) {
+        nums[0] = second;
+        if(first < third) {
+            nums[1] = first;
+            nums[2] = third;
+        }else{
+            nums[1] = third;
+            nums[2] = first;
+        }
+}else if(third < first && third < second) {
+    nums[0] = third;
+    if(first < second) {
+        nums[1] = first;
+        nums[2] = second;
+    }else{
+        nums[1] = second;
+        nums[2] = first;
+    }
+    }
+
+    for(int i = 0; i < 3; i++) {
+        if(order.charAt(i) == 'A') {
+            System.out.print(nums[0] + " ");
+        }else if(order.charAt(i) == 'B') {
+            System.out.print(nums[1] + " ");
+        }else if(order.charAt(i) == 'C') {
+            System.out.print(nums[2] + " ");
+        }
+    }
+}
+}
---
+++ statues.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class statues {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int n = in.nextInt();
+
+        int printers = 1;
+
+        int days = 0;
+
+        while(printers < n) {
+            printers *= 2;
+            days++;
+        }
+
+        System.out.println(days + 1);
+    }
+}
