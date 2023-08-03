1f0c365c71442439c5972d7406a0ea7192e81d68
--global <--global>
2023-08-02 22:50:01 -0500

    it worked

---
+++ aboveAverage.java
@@ -0,0 +1,33 @@
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class aboveAverage {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        DecimalFormat df = new DecimalFormat("#.###");
+        df.setMinimumFractionDigits(3);
+        int cases = in.nextInt();
+
+        for (int i = 0; i < cases; i++) {
+            int n = in.nextInt();
+
+            int total = 0;
+            int count = 0;
+            int[] scores = new int[n];
+
+            for (int j = 0; j < n; j++) {
+                scores[j] = in.nextInt();
+                total += scores[j];
+            }
+            double average = (double) total / n;
+
+            for (int k = 0; k < n; k++) {
+                if (scores[k] > average) {
+                    count++;
+                }
+            }
+
+            System.out.println(df.format((double)count / n * 100) + "%");
+        }
+    }
+}
