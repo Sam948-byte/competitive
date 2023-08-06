6b7d43122304d19d5a423c167e5c8ae12ecbd9ea
--global <--global>
2023-08-05 21:40:08 -0500

    a different problem

---
+++ aDifferentProblem.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class aDifferentProblem {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        while(in.hasNextLong()){
+            long one = in.nextLong();
+            long two = in.nextLong();
+
+            System.out.println(Math.abs(one - two));
+        }
+    }
+}
