4d25f442ef136a658bd1d87da2a35a8cf78bd8bf
Sam <techguy763@gmail.com>
2023-08-15 20:01:19 +0000

    r2

---
+++ r2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class r2 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int r = in.nextInt();
+
+        int s = in.nextInt();
+
+        System.out.print(s * 2 - r);
+    }
+}
