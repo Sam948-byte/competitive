6be413cb5a8944ea0127fda32c2078ea99fe2652
Sam <techguy763@gmail.com>
2023-08-15 20:24:18 +0000

    start directions

---
+++ allDifferentDirections.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class allDifferentDirections {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int n = in.nextInt();
+
+        
+    }
+}
