0f9bd4f5f4fe0e6748d8a0b26638381f211da931
Sam <techguy763@gmail.com>
2023-08-15 17:09:09 +0000

    okviri

---
+++ okviri.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+
+public class okviri {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        String input = in.nextLine();
+
+        int length = input.length();
+
+        topAndBottom(length);
+        middles(length);
+        letter(length, input);
+        middles(length);
+        topAndBottom(length);
+
+    }
+
+    public static void topAndBottom(int length) {
+        System.out.print(".");
+        for (int i = 0; i < length; i++) {
+            System.out.print(".");
+            if ((i + 1) % 3 == 0) {
+                System.out.print("*");
+            } else {
+                System.out.print("#");
+            }
+            System.out.print("..");
+        }
+        System.out.print("\n");
+    }
+
+    public static void middles(int length) {
+        System.out.print(".");
+        for (int i = 0; i < length * 2; i++) {
+            if ((i % 6 + 1) % 5 == 0 || (i % 6+ 1) % 6 == 0) {
+                System.out.print("*");
+            } else {
+                System.out.print("#");
+            }
+            System.out.print(".");
+        }
+        System.out.print("\n");
+    }
+
+    public static void letter(int length, String input) {
+        System.out.print("#");
+        for (int i = 0; i < length; i++) {
+            System.out.print("." + input.charAt(i) + ".");
+
+            if ((i + 1) % 3 == 0 || (i + 1 < length && (i + 1) % 3 == 2)) {
+                System.out.print("*");
+            } else {
+                System.out.print("#");
+            }
+        }
+        System.out.print("\n");
+    }
+}
