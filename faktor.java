c5f02919b35f7f95e5a4638734b86cd1160a2040
Sam <techguy763@gmail.com>
2023-08-15 20:15:44 +0000

    faktor

---
+++ faktor.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class faktor {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int articles = in.nextInt();
+
+        double impact = in.nextInt();
+
+        int scientists = 1;
+
+        double fakeImpact = scientists / articles;
+
+        while (fakeImpact < impact - 1) {
+            scientists++;
+            fakeImpact = scientists / articles;
+        }
+
+        System.out.print(scientists + 1);
+
+    }
+}
