d699209eca74ea6e3923c95715cb790da04f4707
Sam <techguy763@gmail.com>
2023-08-25 15:36:12 +0000

    solve turn it up

---
+++ solved/turnItUp.java
@@ -0,0 +1,29 @@
+package solved;
+
+import java.util.Scanner;
+
+public class turnItUp {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int n = in.nextInt();
+
+        int volume = 7;
+
+        for (int i = 0; i < n * 2; i++) {
+            String order = in.next();
+            if (order.equals("op!")) {
+                volume++;
+            } else if (order.equals("ned!")) {
+                volume--;
+            }
+            if (volume > 10) {
+                volume = 10;
+            } else if (volume < 0) {
+                volume = 0;
+            }
+        }
+
+        System.out.print(volume);
+    }
+}
