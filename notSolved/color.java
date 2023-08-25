444f23cee889b29d4f6821f61c5d0411ecd3dba9
Sam <techguy763@gmail.com>
2023-08-25 15:49:22 +0000

    finish

---
+++ notSolved/color.java
@@ -0,0 +1,23 @@
+package notSolved;
+
+import java.util.Scanner;
+
+public class color {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int n = in.nextInt();
+
+        int c = in.nextInt();
+
+        int k = in.nextInt();
+
+        int[] colors = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            colors[i] = in.nextInt();
+        }
+
+        
+    }
+}
---
+++ notSolved/theGourmet.java
@@ -0,0 +1,21 @@
+package notSolved;
+
+import java.util.Scanner;
+
+public class theGourmet {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int m = in.nextInt();
+
+        int n = in.nextInt();
+
+        int[] times = new int[n];
+
+        for(int i = 0; i < n; i++){
+            times[i] = in.nextInt();
+        }
+
+        
+    }
+}
