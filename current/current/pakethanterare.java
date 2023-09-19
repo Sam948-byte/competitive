37f3a49403433112b61142d1951bd43a82aebaad
Sam <techguy763@gmail.com>
2023-09-19 18:49:51 +0000

    move relatives

---
+++ current/current/pakethanterare.java
@@ -0,0 +1,11 @@
+package current;
+
+import java.util.Scanner;
+
+public class pakethanterare{
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        
+    }
+}
---
+++ solved/mylla2.java
@@ -0,0 +1,45 @@
+package solved;
+
+import java.util.Scanner;
+
+public class mylla2 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        String[][] grid = new String[3][3];
+
+       for(int i = 0; i < 3; i++){
+            grid[i] = in.next().split("");
+       }
+
+        in.close();
+
+        boolean found = false;
+
+        for (int i = 0; i < 3; i++) {
+            if (grid[0][i].equals("O") && grid[1][i].equals("O") && grid[2][i].equals("O")) {
+                System.out.print("Jebb");
+                found = true;
+                break;
+            }
+            if (grid[i][0].equals("O") && grid[i][1].equals("O") && grid[i][2].equals("O")) {
+                System.out.print("Jebb");
+                found = true;
+                break;
+            }
+        }
+
+        if(!found && grid[1][1].equals("O")){
+            if(grid[0][0].equals("O") && grid[2][2].equals("O")){
+                System.out.print("Jebb");
+                found = true;
+            }
+            if(grid[0][2].equals("O") && grid[2][0].equals("O")){
+                System.out.print("Jebb");
+                found = true;
+            }
+        } 
+
+        if(!found) System.out.print("Neibb");
+    }
+}
