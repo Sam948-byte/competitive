eb3e9a2ccd84d039f4cce6cf18b6118c71012272
Sam <techguy763@gmail.com>
2024-01-05 16:25:38 +0000

    completed

---
+++ notSolved/vidsnuningur.java
@@ -0,0 +1,20 @@
+package notSolved;
+
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class vidsnuningur {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        String str = in.next();
+
+        List<Character> strII = str.chars().mapToObj(e -> (char) e).collect(Collectors.toList());
+        
+        for(int i = strII.size() - 1; i > -1; i--){
+            System.out.print(strII.get(i));
+        }
+
+    }
+}
